2ffaa62c0ed7d7076b1dc3be21bf70677df73e59
Simone Basso <bassosimone@gmail.com>
2024-12-10 16:48:46 +0100

    BREAKING CHANGE(sh): only run rbmk & built-in commands
    This diff modifies how `rbmk sh` executes commands by only
    allowing internal commands (`pwd`, `cd`) and `rbmk` commands,
    which are also exported as internal commands.
    
    The main motivation for implementing this change is ensuring
    that a script running on, say, Linux, would also work on, say,
    Windows, without issues caused by missing commands.
    
    Figuring out whether the decision to implement this change was
    motivated by theoretical concerns or hands on experience is
    left as an exercise for the reader :grimacing:.
    
    To preserve backward compatibility, we set `$RBMK_EXE` to `rbmk`
    thus allowing previous script to work as long as they were using
    the `rbmk` command only. Scripts using external commands would
    stop working, hence this is still a breaking change.
    
    See the updated documentation in pkg/cli/sh/README.md for more
    details about this change and guidance on writing portable scripts.

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -8,47 +8,14 @@ import (
 
 	"github.com/rbmk-project/common/cliutils"
 	"github.com/rbmk-project/rbmk/internal/markdown"
-	"github.com/rbmk-project/rbmk/pkg/cli/cat"
-	"github.com/rbmk-project/rbmk/pkg/cli/curl"
-	"github.com/rbmk-project/rbmk/pkg/cli/dig"
-	"github.com/rbmk-project/rbmk/pkg/cli/intro"
-	"github.com/rbmk-project/rbmk/pkg/cli/ipuniq"
-	"github.com/rbmk-project/rbmk/pkg/cli/mkdir"
-	"github.com/rbmk-project/rbmk/pkg/cli/mv"
-	"github.com/rbmk-project/rbmk/pkg/cli/nc"
-	"github.com/rbmk-project/rbmk/pkg/cli/pipe"
-	"github.com/rbmk-project/rbmk/pkg/cli/rm"
+	"github.com/rbmk-project/rbmk/internal/rootcmd"
 	"github.com/rbmk-project/rbmk/pkg/cli/sh"
-	"github.com/rbmk-project/rbmk/pkg/cli/stun"
-	"github.com/rbmk-project/rbmk/pkg/cli/tar"
-	"github.com/rbmk-project/rbmk/pkg/cli/timestamp"
-	"github.com/rbmk-project/rbmk/pkg/cli/tutorial"
-	"github.com/rbmk-project/rbmk/pkg/cli/version"
 )
-
-//go:embed README.md
-var readme string
 
 // NewCommand constructs a new [cliutils.Command] for the `rbmk` command.
 func NewCommand() cliutils.Command {
+	directory := rootcmd.CommandsWithoutSh()
+	directory["sh"] = sh.NewCommand()
 	return cliutils.NewCommandWithSubCommands(
-		"rbmk", markdown.LazyMaybeRender(readme),
-		map[string]cliutils.Command{
-			"cat":       cat.NewCommand(),
-			"curl":      curl.NewCommand(),
-			"dig":       dig.NewCommand(),
-			"intro":     intro.NewCommand(),
-			"ipuniq":    ipuniq.NewCommand(),
-			"mkdir":     mkdir.NewCommand(),
-			"mv":        mv.NewCommand(),
-			"nc":        nc.NewCommand(),
-			"pipe":      pipe.NewCommand(),
-			"rm":        rm.NewCommand(),
-			"sh":        sh.NewCommand(),
-			"stun":      stun.NewCommand(),
-			"tar":       tar.NewCommand(),
-			"timestamp": timestamp.NewCommand(),
-			"tutorial":  tutorial.NewCommand(),
-			"version":   version.NewCommand(),
-		})
+		"rbmk", markdown.LazyMaybeRender(rootcmd.HelpText()), directory)
 }
