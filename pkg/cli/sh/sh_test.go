af8ed87d7741d3a38214f39832875aa55166fdc9
Simone Basso <bassosimone@gmail.com>
2025-03-09 10:00:17 +0100

    x

---
+++ pkg/cli/sh/sh_test.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package sh
+
+import (
+	"os"
+	"testing"
+)
+
+// Ensure that `$RBMK_EXE` expands to `rbmk` in the common case.
+func Test_osGetenvWrapper(t *testing.T) {
+	// Make sure the variable is not set already
+	const varname = "RBMK_EXE"
+	if os.Getenv(varname) != "" {
+		t.Skip("the environment variable is already set")
+	}
+
+	// Check whether we get the expected value (yes, there is a race
+	// with a potential os.Setenv in this test but we're not going
+	// to call os.Setenv here so we're good).
+	if value := osGetenvWrapper(varname); value != "rbmk" {
+		t.Error("unexpected value", value)
+	}
+}
